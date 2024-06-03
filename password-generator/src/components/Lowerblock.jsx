86910b3062ba592d1d7c0b0c1cea1effe7c8f49e
karma2912 <rajakyash23@gmail.com>
2024-06-04 00:26:28 +0530

    half changes made

---
+++ password-generator/src/components/Lowerblock.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Lowerblock() {
+  return (
+    <>
+    <div className='main-lower'>
+      <div className="firstHalf">
+      <span className='char'>
+     Character Length
+    </span>
+    <span className="num-output">
+     num
+    </span>
+      </div>
+      <div className='container'>
+        <input type='range' className='range'></input>
+        </div>
+        <div className='secondHalf'>
+     <div className='check-boxes'>
+      <input type='checkbox' className='check'/>Include Uppercase Letter<br/>
+      <input type='checkbox' className='check'/>Include Lowercase Letter<br/>
+      <input type='checkbox' className='check'/>Include Symbols<br/>
+      <input type='checkbox' className='check'/>Include Numbers<br/>
+        </div>
+        </div>
+        </div>
+     </>
+  )
+}
---
+++ password-generator/src/components/Upperblock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Upperblock() {
+  return (
+    <>
+    <div className='output'>
+    Hiii i am from lowerBLock
+    </div>
+    <div className='copy-btn'>
+    btn
+    </div>
+    </>
+  )
+}
